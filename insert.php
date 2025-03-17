449b35cf1d26e606dcb22f60d9373a9850a2eb75
dilaw <154446517+siopaoboy@users.noreply.github.com>
2025-03-16 22:41:58 -0500

    worked on the main page of the website, added a navigational bar

---
+++ insert.php
@@ -0,0 +1,28 @@
+<?php
+
+	include('search.php');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+
+
+	<title>MANGAKOPIA ~ Winnipeg's Go-To Shop For Anything Manga!</title>
+</head>
+<body class="index">
+	
+	<a href="login.php">Login </a> <br>
+	<a href="register.php">Become a MANGAKOPIA Member!</a> <br>
+	<?= include('nav.php'); ?> <br>
+
+	<div class="boba">
+	<h2><u>Welcome to MANGAKOPIA</u></h2> <br>
+		<p>We are here to  </p>
+	</div>
+</body>
+</html>
