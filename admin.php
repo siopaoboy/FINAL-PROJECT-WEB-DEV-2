2e08d3442327966403058bd68cf757e7dab8e289
dilaw <154446517+siopaoboy@users.noreply.github.com>
2025-03-30 01:40:35 -0500

    added admin register and login pages

---
+++ admin.php
@@ -0,0 +1,69 @@
+<?php
+
+	require('connect.php');
+
+	if (!empty($_POST['usernameRegister']) && !empty($_POST['passwordRegister']) && !empty($_POST['passwordConfirmation'])) {
+
+		$username = filter_input(INPUT_POST, 'usernameRegister', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$password = filter_input(INPUT_POST, 'passwordRegister', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$reenter = filter_input(INPUT_POST, 'passwordConfirmation', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+
+		if ($password === $reenter) {
+
+			$hash = password_hash($password, PASSWORD_DEFAULT); 
+			
+        	$query = "INSERT INTO admin (username, password) VALUES (:username, :password)";
+
+        	$statement = $db->prepare($query);
+        
+        	$statement->bindValue(':username', $username);
+        	$statement->bindValue(':password', $hash);
+        
+        	try {
+        		$statement->execute();
+        		$message = "Registration successful!";
+        	} catch (PDOException $e) {
+        		$message = "Error: cannot register your account " . $e -> getMessage();
+        	}
+       		
+    	} else {
+    		$message = "The passwords do not match";
+    	}  	
+
+	} else {
+		$message = "Please enter a username and password";
+
+	} 	
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/register.css">
+	<title>Admin Register </title>
+</head>
+<body>
+	<h1>Admin Page</h1>
+ 
+	<p><?= $message ?></p>
+
+	<form method="POST" action="admin.php">
+		<label for="usernameRegister">Username:</label>
+		<input type="text" name="usernameRegister"> <br> <br>
+
+		<label for="passwordRegister">Password:</label>
+		<input type="password" name="passwordRegister"> <br> <br>
+
+		<label for="reenter">Re-Enter Password:</label>
+		<input type="password" name="passwordConfirmation"> <br> <br>
+
+		<button name="register" type="submit">Register</button> 
+	</form> <br>
+
+	<p> <a href="adminLogin.php">Login as an admin here</a>
+</body>
+</html>
---
+++ adminLogin.php
@@ -0,0 +1,76 @@
+<?php
+	
+	require('connect.php');
+
+	$message = "";
+
+	session_start();
+
+	if (isset($_POST['username']) && isset($_POST['password'])) {
+
+		$user = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$pass = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+		if (empty($user) || empty($pass)) {
+
+			$message = "Please enter both username and password";
+
+		} else {
+
+		$login = "SELECT username, password FROM admin WHERE username = :username";
+		$statement = $db->prepare($login);
+		$statement->bindValue(':username', $user);
+		$statement->execute();
+
+			if ($statement->rowCount() > 0) {
+
+				$row = $statement->fetch();
+
+				$hash = password_hash($pass, PASSWORD_DEFAULT);
+
+				if(password_verify($pass, $hash)){
+
+					$session = $_SESSION['username'];
+
+					setcookie('username', $session, time() + 300);
+					$message = "Login Successful!";
+
+					header("Location: insert.php");
+					exit();
+
+				} else {
+					$message = "Wrong password";
+				}
+
+			} else {
+				$message = "Username and Password is invalid. Please try again.";
+			}
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sign In</title>
+</head>
+<body>
+	<h1>Admin Sign-in</h1>
+
+	<p> <?= $message ?> </p>
+
+	<form method="POST" action="adminLogin.php">
+		<label for="username">Username:</label> 
+		<input type="text" name="username"> <br> <br>
+
+		<label for="password">Password:</label>
+		<input type="password" name="password"> <br> <br>
+
+		<button name="login" type="submit">Sign In</button>
+	</form>
+
+	<p> <a href="admin.php">Register as admin</a>
+</body>
+</html>
