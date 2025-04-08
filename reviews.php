53c48bba789b23fbc52f3e943c6051899553c341
dilaw <154446517+siopaoboy@users.noreply.github.com>
2025-04-08 11:51:49 -0500

    improved on the reviews page that when a user is not logged in, they will not be able to submit a review unless they are logged in

--- reviews.php
+++ reviews.php
@@ -3,13 +3,16 @@
     require('nav.php');
 
     session_start();
+    $message = "";
 
     if ($_POST && !empty($_POST['title']) && !empty($_POST['content'])) {
 
         $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        // $content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $content = $_POST['content'];
-        
+
+        if (empty($_SESSION['username'])) {
+            $message = "Please login to submit a review.";
+        } else {
         $query = "INSERT INTO reviews (user_id, title, content) VALUES (:user_id, :title, :content)";
         $statement = $db->prepare($query);
         
@@ -19,7 +22,11 @@
         
         $statement->execute();
 
-        header("Location: insert.php");
+        $message = "Review has been submitted! Check it out at the home page.";
+        }
+
+    } else {
+        $message = "Either title or review is left empty. Type something down on both fields";
     }
 ?>
 
@@ -36,8 +43,7 @@
     <script src="/WEBD2/PROJECT-WEB-DEV-2/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
       tinymce.init({
-        selector: '#mytextarea',
-        toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+        selector: '#mytextarea'
       });
     </script>
 </head>
@@ -45,10 +51,11 @@
 <body class="reviews">
     <h2>My Review</h2> <br>
     <form method="post" action="reviews.php">
+        <?= $message ?> <br> <br>
         <h5><label for="title">Title:</label></h5>
         <input id="title" name="title" placeholder="Add a title"> <br> <br>
 
-        <h4><label for="content">Content:</label></h4> 
+        <h4><label for="content">Review:</label></h4> 
         <textarea id="mytextarea" name="content" rows="5" cols="50" placeholder="What do you think of MANGAKOPIA?"></textarea> <br> <br>
 
         <input class="btn btn-primary" type="submit" value="Post Review">
